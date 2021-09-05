0fa5baa35faa9e33a527fd1a1d01afd76dcf29f5
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-09-05 09:38:03 +0800

    ruby_array_initialization

---
+++ ruby_array_initialization.rb
@@ -0,0 +1,19 @@
+# Initialize an empty array with the variable name array
+
+array = Array.new
+# or
+array = []
+
+# Initialize an array with exactly one nil element in it with the variable name array_1
+
+array_1 = Array.new(1)
+# or
+array_1 = [nil]
+
+# Initialize an array with exactly two elements with value 10 in it using the variable name array_2.
+
+array_2 = Array.new(2, 10)
+# or
+array_2 = [10,10]
+
+
