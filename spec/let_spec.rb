a155d6ce5c1abee2bac757daaa07dc2545ff468c
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-09-23 15:21:34 +0800

    update cases in let_spec.rb

--- spec/let_spec.rb
+++ spec/let_spec.rb
@@ -4,7 +4,7 @@
 var = 0
 describe "let" do
   let(:count) { var += 1 }
-  let(:name) {'ariffnorhadi'}
+  let(:name) { 'ariffnorhadi' }
 
   it "memoizes the value" do
     expect(count).to be 1
@@ -15,6 +15,35 @@ describe "let" do
   it "is not cached across examples" do
     expect(count).to be 2
     name = 'anaknorhadi'
-    expect(name).to eql 'ariffnorhadi'
+    expect(name).to eql 'anaknorhadi'
+  end
+end
+
+# References
+# https://mixandgo.com/learn/let-vs-instance-variables-in-rspec
+
+describe Time do
+  let(:current_time) { Time.now }
+  let!(:current_time_let!) { Time.now }
+
+  before do
+    puts Time.now
+  end
+
+  it 'gets the same time over and over again' do
+    puts "current_time => #{current_time}"
+    sleep(2)
+    puts "current_time_let! => #{current_time_let!}"
+  end
+
+  it 'gets the time again' do
+    puts current_time
+  end
+
+  it 'not get the same value after some time' do
+    sleep(3)
+    puts "current time => #{current_time}"
+    puts "current time let! => #{current_time_let!}"
+    expect(current_time_let!).to_not eq(current_time)
   end
 end
