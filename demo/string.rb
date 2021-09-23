edb2fc2e3b99ef5e2924c8bf1d8cc37f83ab3249
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-09-23 10:59:44 +0800

    try freeze method for string

---
+++ demo/string.rb
@@ -0,0 +1,6 @@
+string = 'salam'.freeze
+string = 'hello' # will not work as the variable is frozen , refer line 1
+puts string
+
+# References
+# https://stackoverflow.com/questions/27664050/ruby-freeze-method
