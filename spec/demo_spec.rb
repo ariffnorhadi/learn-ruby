fdb30a40874c99f96673991b639df5721216a354
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-10-06 17:09:41 +0800

    update demo_spec.rb

--- spec/demo_spec.rb
+++ spec/demo_spec.rb
@@ -18,13 +18,23 @@ end
 
 RSpec.describe Demo do
 
-  let(:message) { 'number is more than one '}
-  let(:number) { 2 }
+  context "when number is more than one" do
+    let(:message) { 'number is more than one'}
+    let(:number) { 2 }
 
-  subject { described_class.new(string: 'message', number: 2) }
-
-  it 'should return number more than one' do
-    expect()
+    it 'should return number more than one' do
+      test = Demo.new(message, number)
+      expect(test.fail_message).to eq(message)
+    end
   end
 
+  context "when number is not more than one" do
+    let(:message) { 'number is not more than one'}
+    let(:number) { 1 }
+
+    it "should return number is not more than one" do
+      test = Demo.new(message, number)
+      expect(test.fail_message).to eq(message)
+    end
+  end
 end
