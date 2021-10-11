8d1ec9ec8dbb06d5b82bdc8ead408625c3f8bd9e
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-10-11 10:27:27 +0800

    if else without else

--- demo/conditional.rb
+++ demo/conditional.rb
@@ -5,3 +5,12 @@ if i > 1
 else
   puts "#{i} is less than 1"
 end
+
+# so we can put if else (i mean without else)
+j = 5
+
+if j > 7
+  puts "#{j} is more than five"
+elsif j == 7
+  puts "#{j} is seven"
+end
