f4a9feefeb586bd9c3513f5e7f070840f463c365
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-10-26 15:08:38 +0800

    modify string while looping

---
+++ demo/sms_service.rb
@@ -0,0 +1,24 @@
+class SMSService
+  def initialize(sms_id, date)
+    @sms_id = sms_id
+    @date = date
+  end
+
+  def self.send_sms(to, text)
+    puts "send sms to = #{to} with content of #{text}"
+  end
+end
+
+SMSService.send_sms(01233333, "hi this is the content of the message")
+
+error_messages = ""
+
+(0..5).each do |index|
+  error_messages+= "#{index.to_s},"
+end
+
+puts error_messages
+# puts ''
+# 6.times do |index|
+#   puts index
+# end
